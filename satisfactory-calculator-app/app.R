368c3dd71c2c24839c4d459af712ab2af51228b9
Sedat Demiriz <sedatdemiriz97@gmail.com>
2022-09-06 19:33:56 -0700

    Added fluidRow layout to page + simplified look

--- satisfactory-calculator-app/app.R
+++ satisfactory-calculator-app/app.R
@@ -8,6 +8,9 @@ unique_product_names = RECIPES %>%
                           arrange(decreasing=TRUE) %>%
                           unique()
 
+side_panel_width = 3
+main_panel_width = 12 - side_panel_width
+
 ui <- fluidPage(
   
   # App title ----
@@ -15,71 +18,70 @@ ui <- fluidPage(
   
   # Responsive sidebar
   # items filter dropdown (selectInput) - DONE
-  # recipes filter dropdown, values reactive to items filter (selectInput)
-  # quantity input field (numericInput)
+  # recipes filter dropdown, values reactive to items filter (selectInput) - DONE
+  # quantity input field (numericInput) - DONE
   
   # Responsive main panel
   # table responsive to dropdowns in sidebar (renderDataTable)
   
-  sidebarLayout(
-    sidebarPanel(
-      # Item filter
-      selectInput(inputId='item_filter', 
-                  label='Select Item', 
-                  choices=unique_product_names),
-      
-      # Recipe filter based on item filter (unimplemented)
-      # selectInput(inputId='recipe_filter', 
-      #             label='Select Recipe', 
-      #             choices=character(0)),
-      
-      uiOutput('recipe_filter'),
-      
-      # Quantity input (unimplemented)
-      numericInput(inputId='quantity',
-                   label='Quantity',
-                   value=0)
+  fluidRow(
+    
+      column(side_panel_width,
+             
+          wellPanel(
+            
+          # Item filter
+          selectizeInput(inputId='item_filter', 
+                      label='Select Item', 
+                      choices=unique_product_names),
+          
+          # Placeholder for when this UI is generated based on selected item
+          uiOutput('recipe_filter'),
+          
+          # Amount of selected item to produce using the selected recipe
+          numericInput(inputId='quantity',
+                       label='Quantity',
+                       value=0)
+          )
       ),
-    # Main panel for displaying outputs ----
-    mainPanel(
+
+      column(main_panel_width,
       
-      # Output: Histogram ----
-      dataTableOutput('recipes_table')
-    )
+          tableOutput('recipes_table')
+      )
   )
 )
 
 # Define server logic required to draw a histogram ----
 server <- function(input, output, session) {
   
-  output$recipe_filter = renderUI({
+    output$recipe_filter = renderUI({
     
-    unique_recipe_names = RECIPES %>%
-                            filter(product == input$item_filter) %>%
-                            select(recipe) %>%
-                            arrange(decreasing=TRUE)
+        unique_recipe_names = RECIPES %>%
+                                filter(product == input$item_filter) %>%
+                                select(recipe) %>%
+                                arrange(decreasing=TRUE)
     
-    selectInput('recipe_filter', 
-                'Select Recipe', 
-                unique_recipe_names)
+        selectizeInput('recipe_filter', 
+                    'Select Recipe', 
+                    unique_recipe_names)
   })
   
-  output$recipes_table = renderDataTable({
+    output$recipes_table = renderTable({
     
-    unique_recipe_names = RECIPES %>%
-                            select(recipe, 
-                                   input_1, input_rate_1, 
-                                   input_2, input_rate_2, 
-                                   input_3, input_rate_3, 
-                                   input_4, input_rate_4, 
-                                   building, 
-                                   product, product_rate, 
-                                   byproduct, byproduct_rate) %>%
-                            filter(product == input$item_filter) %>%
-                            arrange(recipe, decreasing=TRUE)
+          unique_recipe_names = RECIPES %>%
+                                  select(recipe, 
+                                         input_1, input_rate_1, 
+                                         input_2, input_rate_2, 
+                                         input_3, input_rate_3, 
+                                         input_4, input_rate_4, 
+                                         building, 
+                                         product, product_rate, 
+                                         byproduct, byproduct_rate) %>%
+                                  filter(product == input$item_filter) %>%
+                                  arrange(recipe, decreasing=TRUE)
     
     })
-  
 }
 
 shinyApp(ui=ui, server=server)
