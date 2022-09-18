552b906594f1193f544451587114c00fa7fdeaf3
Sedat Demiriz <sedatdemiriz97@gmail.com>
2022-09-17 22:23:07 -0700

    Added autoreload support for quicker iteration

--- runApp.r
+++ runApp.r
@@ -2,6 +2,7 @@ library(shiny)
 library(tidyverse)
 
 options(shiny.port = 8888)
+options(shiny.autoreload = TRUE)
 
 # Run app located in specified dir at specified port
 runApp('satisfactory-calculator-app', port = getOption("shiny.port"))
