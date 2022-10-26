4d29504f888d447ca14cd0a5990046a84209c153
Sedat Demiriz <sedatdemiriz97@gmail.com>
2022-10-25 23:20:02 -0700

    Simplified Search Item Filter module

--- satisfactory-calculator-app/modules/searchItemFilter.R
+++ satisfactory-calculator-app/modules/searchItemFilter.R
@@ -3,33 +3,24 @@ library(shiny)
 searchItemFilterUI <- function(id) {
   
   ns <- NS(id)
-  uiOutput(ns('search_item_filter'))
+  unique_item_names <- RECIPES %>% 
+                        select(product) %>%
+                        arrange(product) %>%
+                        unique() %>%
+                        pull()
+
+  tagList(
+    selectInput(
+      inputId = ns('selected_item'), 
+      label = 'Select Item to start Crafting Tree',
+      choices = unique_item_names
+    )
+  )
 }
 
-searchItemFilterServer <- function(id, RECIPES) {
+searchItemFilterServer <- function(id) {
   moduleServer(id, function(input, output, session) {
-      
-    output$search_item_filter <- renderUI({
-      
-      ns <- session$ns
-      
-      # Find all item names available in provided data
-      item_names <- RECIPES %>%
-                      select(product) %>%
-                      arrange(product) %>%
-                      unique() %>%
-                      pull()
-      
-      return(
-        selectInput(
-          inputId = ns('selected_item'), 
-          label = 'Select Item to start Crafting Tree',
-          choices = item_names
-        )
-      )
-    })
-    
-    return(reactive({ input$selected_item }))
+    reactive(input$selected_item)
   })
 }
 
