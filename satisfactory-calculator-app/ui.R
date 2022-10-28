8369417036e1bc9421e76c835310dacde2c5c92a
Sedat Demiriz <sedatdemiriz97@gmail.com>
2022-10-27 23:26:14 -0700

    Added debugging component

--- satisfactory-calculator-app/ui.R
+++ satisfactory-calculator-app/ui.R
@@ -22,8 +22,9 @@ ui <- fluidPage(
     column(FULL_SCREEN_WIDTH, h3('Search for Recipes')),
     
     # Small part of the screen width
-    column(SIDE_PANEL_WIDTH, searchSidebarUI('search_sidebar')
-  ),
+    column(SIDE_PANEL_WIDTH, searchSidebarUI('search_sidebar'),
+             # textOutput('debugger')
+    ),
     
     # Remaining width of screen houses the "Search Results" table
     column(MAIN_PANEL_WIDTH, searchRecipesTableUI('search_recipes_table'))
@@ -31,27 +32,27 @@ ui <- fluidPage(
   
   # Row 3: Crafting Tree and its sidebar
   fluidRow(
-    
+
     # Subtitle for section
     column(FULL_SCREEN_WIDTH, h3('Complete Crafting Tree')),
-    
+
     # Small part of screen width is take up by the recipe selector
     column(SIDE_PANEL_WIDTH,
-           
+
       # Make recipe selector fields appear grouped
       wellPanel(
-      
+
         # Item selector for Crafting Tree
         craftingInputFilterUI('crafting_input_filter'),
-        
+
         # Recipe selector for Crafting Tree
         craftingRecipeFilterUI('crafting_recipe_filter'),
-        
+
         # Confirm adding the item, recipe to Crafting Tree
         craftingConfirmButtonUI('crafting_confirm_button')
       )
     ),
-    
+
     # Remaining width of the screen houses the "Crafting Tree" table
     column(MAIN_PANEL_WIDTH, tableOutput('crafting_table'))
   )
