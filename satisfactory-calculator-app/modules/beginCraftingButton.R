9ad1c5429b399aae6770c114b2ac84408c9975ca
Sedat Demiriz <sedatdemiriz97@gmail.com>
2022-10-27 22:10:42 -0700

    Simplified beginCraftingButton

--- satisfactory-calculator-app/modules/beginCraftingButton.R
+++ satisfactory-calculator-app/modules/beginCraftingButton.R
@@ -3,24 +3,17 @@ library(shiny)
 beginCraftingButtonUI <- function(id) {
   
   ns <- NS(id)
-  uiOutput(ns('search_crafting_start'))
+  tagList(
+    actionButton(
+      inputId = 'crafting_start', 
+      label = 'Begin crafting tree'
+    )
+  )
 }
 
 beginCraftingButtonServer <- function(id) {
   moduleServer(id, function(input, output, session) {
-    
-    output$search_crafting_start <- renderUI({
-      
-      ns <- session$ns
-      return(
-        actionButton(
-          inputId = 'crafting_start', 
-          label = 'Begin crafting tree'
-        )
-      )
-    })
-    
-    return(reactive({ input$crafting_start }))
+    reactive(input$search_crafting_start)
   })
 }
 
