b035a0d0a7923f87fe313536c2a58a332f95d656
Sedat Demiriz <sedatdemiriz97@gmail.com>
2022-10-27 22:11:07 -0700

    Simplified searchItemQuantity

--- satisfactory-calculator-app/modules/searchItemQuantity.R
+++ satisfactory-calculator-app/modules/searchItemQuantity.R
@@ -3,26 +3,18 @@ library(shiny)
 searchItemQuantityUI <- function(id) {
   
   ns <- NS(id)
-  uiOutput(ns('search_item_quantity'))
+  tagList(
+    numericInput(inputId = ns('selected_quantity'),
+                  label = 'Select Quantity of Item',
+                  value = 10,
+                  min = 1
+    )
+  )
 }
 
 searchItemQuantityServer <- function(id) {
   moduleServer(id, function(input, output, session) {
-    
-    output$search_item_quantity <- renderUI({
-      
-      ns <- session$ns
-      return(
-        numericInput(
-          inputId = ns('selected_quantity'),
-          label = 'Select Quantity of Item',
-          value = 10,
-          min = 1
-        )
-      )
-    })
-    
-    return(reactive({ input$selected_quantity }))
+    reactive(input$selected_quantity)
   })
 }
 
