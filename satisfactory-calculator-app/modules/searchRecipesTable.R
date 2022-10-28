d0aba2c76dcfea1f0a158643f7fa8ad62aca81cc
Sedat Demiriz <sedatdemiriz97@gmail.com>
2022-10-27 22:12:21 -0700

    Added reactive input/output functionality to searchRecipesTable

--- satisfactory-calculator-app/modules/searchRecipesTable.R
+++ satisfactory-calculator-app/modules/searchRecipesTable.R
@@ -6,7 +6,8 @@ searchRecipesTableUI <- function(id) {
   uiOutput(ns('search_recipes_table'))
 }
 
-searchRecipesTableServer <- function(id, RECIPES, selected_item) {
+searchRecipesTableServer <- function(id, RECIPES, 
+                                     selected_item, selected_quantity) {
   moduleServer(id, function(input, output, session) {
     
     # Search Bar Table Viewer
@@ -14,22 +15,28 @@ searchRecipesTableServer <- function(id, RECIPES, selected_item) {
       
       ns <- session$ns
       
-      # Select crafting-relevant columns from data
-      unique_recipe_names <- RECIPES %>%
-                              select(
-                                recipe,
-                                input_1, input_rate_1,
-                                input_2, input_rate_2,
-                                input_3, input_rate_3,
-                                input_4, input_rate_4,
-                                building,
-                                product, product_rate,
-                                byproduct, byproduct_rate
-                              ) %>%
-                              filter(product == selected_item()) %>%
-                              arrange(recipe)
+      # Select filtered columns from data
+      recipes_table <- RECIPES %>%
+                        select(
+                          recipe,
+                          input_1, input_rate_1,
+                          input_2, input_rate_2,
+                          input_3, input_rate_3,
+                          input_4, input_rate_4,
+                          building,
+                          product, product_rate,
+                          byproduct, byproduct_rate
+                        ) %>%
+                        filter(product == selected_item()) %>%
+                        arrange(recipe)
       
-      return(unique_recipe_names)
+      # Calculate the ratio (how many times the recipe is to be used for the step)
+      ratio <- .CalculateRatio(selected_quantity(), recipes_table$product_rate)
+      
+      # Apply ratio to inputs, product and byproduct
+      recipes_table <- ApplyRatio(recipes_table, ratio)
+      
+      recipes_table
     })
   })
 }
