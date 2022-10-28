f78329a1679f1ff1683b2f3d9d4193db48a22867
Sedat Demiriz <sedatdemiriz97@gmail.com>
2022-10-27 22:10:52 -0700

    Simplified clearCraftingButton

--- satisfactory-calculator-app/modules/clearCraftingButton.R
+++ satisfactory-calculator-app/modules/clearCraftingButton.R
@@ -3,24 +3,17 @@ library(shiny)
 clearCraftingButtonUI <- function(id) {
   
   ns <- NS(id)
-  uiOutput(ns('search_crafting_clear'))
+  tagList(
+    actionButton(
+      inputId = 'crafting_clear', 
+      label = 'Clear crafting tree'
+    )
+  )
 }
 
 clearCraftingButtonServer <- function(id) {
   moduleServer(id, function(input, output, session) {
-    
-    output$search_crafting_clear <- renderUI({
-      
-      ns <- session$ns
-      return(
-        actionButton(
-          inputId = 'crafting_clear', 
-          label = 'Clear crafting tree'
-        )
-      )
-    })
-    
-    return(reactive({ input$crafting_clear }))
+    reactive(input$search_crafting_clear)
   })
 }
 
