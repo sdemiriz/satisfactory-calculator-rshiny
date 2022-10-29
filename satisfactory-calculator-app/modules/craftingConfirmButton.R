64f96db305d470810ec1ab973f2cf9a0ea724c1c
Sedat Demiriz <sedatdemiriz97@gmail.com>
2022-10-28 21:27:48 -0700

    Simplified craftingConfirmButton

--- satisfactory-calculator-app/modules/craftingConfirmButton.R
+++ satisfactory-calculator-app/modules/craftingConfirmButton.R
@@ -3,24 +3,16 @@ library(shiny)
 craftingConfirmButtonUI <- function(id) {
   
   ns <- NS(id)
-  uiOutput(ns('crafting_confirm_button'))
+  tagList(
+    actionButton(
+      inputId = ns('crafting_confirm_selection'),
+      label = 'Confirm recipe'
+    )
+  )
 }
 
 craftingConfirmButtonServer <- function(id) {
   moduleServer(id, function(input, output, session) {
-    
-    output$crafting_confirm_button <- renderUI({
-      
-      ns <- session$ns
-      
-      return(
-        actionButton(
-          inputId = ns('crafting_confirm'),
-          label = 'Confirm selections'
-        )
-      )
-    })
-    
-    return(reactive({ input$crafting_confirm }))
+    reactive(input$crafting_confirm_selection)
   })
 }
